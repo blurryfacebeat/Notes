ffd4f58d9d2453f889eaa724358563f38b3fe279
Vyacheslav Morozov <31694577+blurryfacebeat@users.noreply.github.com>
2020-06-21 21:43:00 +0700

    Create localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,11 @@
+// Добавляем запись в localStorage(Ключ - Значение).
+localStorage.setItem('number', 1);
+
+// Получаем запись из localStorage.
+console.log(localStorage.getItem('number'));
+
+// Удаление записи.
+localStorage.removeItem('number');
+
+// Полностью очищаем localStorage.
+// localStorage.clear();
