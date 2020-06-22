ff27937d35737e1635095fd85f896b439ed25d1e
Vyacheslav Morozov <31694577+blurryfacebeat@users.noreply.github.com>
2020-06-22 18:19:13 +0700

    Create split.js

---
+++ split.js
@@ -0,0 +1,7 @@
+const str = 'Гена, Алена, Гиена';
+// Разбиваем строку на массив, разделитесь ', '.
+const arr = str.split(', ');
+console.log(str);
+console.log(arr);
+// Склеиваем массив в строку с разделителем ','.
+console.log(arr.join(', '));
