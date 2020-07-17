7e0df0321f70709bb3c23bec58067fbe25506888
Vyacheslav Morozov <31694577+blurryfacebeat@users.noreply.github.com>
2020-07-17 16:10:50 +0700

    Update webpack.config.js

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -15,6 +15,30 @@ module.exports = {
     // Хранит информацию об исходном коде до минификации и т.д.
     devtool: "source-map",
     // Подключаемые дополнительные модули и их настройки
-    module: {}
+    module: {
+        rules: [
+            // Подключаем Babel
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bowe_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', {
+                            targets: {
+                                "edge": "17",
+                                "firefox": "60",
+                                "chrome": "67",
+                                "safari": "11.1"
+                            },
+                            debug: true,
+                            corejs: 3,
+                            useBuiltIns: "usage"
+                        }]]
+                    }
+                }
+            }
+        ]
+    }
     // Есть свойство plugins для подключения различных плагинов
 };
